f11ac0d32bae90be2ff7e7bbb7cf2dc0cc2c7c3d
Thomas Epperson <thomas.epperson@gmail.com>
2026-01-30 12:01:49 -0600

    Fix the api struct.

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,8 @@
 //! Struct definitions used in the api
 
 /// Defines the list of applets that can be worked with
-#[derive(Debug, Copy, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct AppletList {
     /// The list of valid applet ids
-    applet_ids: Vec<i64>,
+    pub applet_ids: Vec<i64>,
 }
